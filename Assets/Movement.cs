37f65da35fce4ba2ea95edd9a717bf9c9924ca4c
benjaminaronson <benjician88@gmail.com>
2024-04-11 16:58:13 -0400

    test1

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //just a comment
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
